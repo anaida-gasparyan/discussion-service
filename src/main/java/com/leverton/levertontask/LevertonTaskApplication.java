159ed6595415ad5b1830817647f15d03f416e0d1
Anaida Gasparyan <anaida.gasparyan@bonial.com>
2018-06-30 16:09:01 +0200

    Initial commit

---
+++ src/main/java/com/leverton/levertontask/LevertonTaskApplication.java
@@ -0,0 +1,12 @@
+package com.leverton.levertontask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevertonTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevertonTaskApplication.class, args);
+	}
+}
